e633b77ea1487a07f08146788f71267a1422d9af
hooyao <HooYao@gmail.com>
2011-09-04 15:53:25 +0000

    -add acm content handler

---
+++ TaxonomyCategorizor/src/java/com/minetool/tax/model/data/ACMContentHandler.java
@@ -0,0 +1,122 @@
+/**
+ * 2011 HuYao
+ */
+package com.minetool.tax.model.data;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+/**
+ * @author HuYao
+ *
+ */
+public class ACMContentHandler implements ContentHandler {
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#characters(char[], int, int)
+     */
+    @Override
+    public void characters(char[] ch, int start, int length)
+	    throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#endDocument()
+     */
+    @Override
+    public void endDocument() throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+     */
+    @Override
+    public void endElement(String uri, String localName, String qName)
+	    throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#endPrefixMapping(java.lang.String)
+     */
+    @Override
+    public void endPrefixMapping(String prefix) throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#ignorableWhitespace(char[], int, int)
+     */
+    @Override
+    public void ignorableWhitespace(char[] ch, int start, int length)
+	    throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void processingInstruction(String target, String data)
+	    throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#setDocumentLocator(org.xml.sax.Locator)
+     */
+    @Override
+    public void setDocumentLocator(Locator locator) {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#skippedEntity(java.lang.String)
+     */
+    @Override
+    public void skippedEntity(String name) throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#startDocument()
+     */
+    @Override
+    public void startDocument() throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     */
+    @Override
+    public void startElement(String uri, String localName, String qName,
+	    Attributes atts) throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#startPrefixMapping(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void startPrefixMapping(String prefix, String uri)
+	    throws SAXException {
+	// TODO Auto-generated method stub
+
+    }
+
+}
