ba6a7e667349ee92328a365b71f6bedb0e9aa023
HooYao@gmail.com <HooYao@gmail.com@252de7e7-0c1b-21dd-e54f-8b5a5f63cec4>
2011-09-05 19:00:24 +0000

    - roughly completed parser

--- trunk/TaxonomyCategorizor/src/java/com/minetool/tax/model/TaxNode.java
+++ trunk/TaxonomyCategorizor/src/java/com/minetool/tax/model/TaxNode.java
@@ -8,18 +8,46 @@ import java.util.List;
  * @version $Revision: 1.0 $
  */
 public abstract class TaxNode {
+    protected String _path = ""; //$NON-NLS-1$
+
+    /**
+     * 
+     * @return the _path
+     */
+    public String getPath() {
+	return this._path;
+    }
+
+    /**
+     * @param path
+     *            the _path to set
+     */
+    public void setPath(String path) {
+	this._path = path;
+    }
+
     protected String _text = ""; //$NON-NLS-1$
     protected List<TaxNode> _taxList = new ArrayList<TaxNode>();
     protected TaxNode _parent;
+
+    /**
+     * Method getParent.
+     * 
+     * @return TaxNode
+     */
     public TaxNode getParent() {
-        return _parent;
+	return this._parent;
     }
 
     /**
      * Constructor for TaxNode.
-     * @param text String
+     * 
+     * @param text
+     *            String
+     * @param parent
+     *            TaxNode
      */
-    public TaxNode(String text,TaxNode parent) {
+    public TaxNode(String text, TaxNode parent) {
 	this._text = text;
 	this._parent = parent;
     }
@@ -27,8 +55,9 @@ public abstract class TaxNode {
     /**
      * Method getText.
      * 
-    
-     * @return String */
+     * 
+     * @return String
+     */
     protected String getText() {
 	return this._text;
     }
@@ -46,8 +75,9 @@ public abstract class TaxNode {
     /**
      * Method getChildList.
      * 
-    
-     * @return List<TaxNode> */
+     * 
+     * @return List<TaxNode>
+     */
     public List<TaxNode> getChildList() {
 	return this._taxList;
     }
@@ -61,4 +91,15 @@ public abstract class TaxNode {
     public void setChildList(List<TaxNode> inList) {
 	this._taxList = inList;
     }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	return getText();
+    }
+
 }
