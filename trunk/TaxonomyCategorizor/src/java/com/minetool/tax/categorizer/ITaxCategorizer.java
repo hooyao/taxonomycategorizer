4b64cfd7b68692874a76261e92bb27e3d7746162
HooYao@gmail.com <HooYao@gmail.com@252de7e7-0c1b-21dd-e54f-8b5a5f63cec4>
2011-09-06 15:25:03 +0000

    - comments amendment

--- trunk/TaxonomyCategorizor/src/java/com/minetool/tax/categorizer/ITaxCategorizer.java
+++ trunk/TaxonomyCategorizor/src/java/com/minetool/tax/categorizer/ITaxCategorizer.java
@@ -14,11 +14,11 @@ import java.util.List;
 public interface ITaxCategorizer {
 
     /**
-     * @param taxList
-     * @param depth
-    
-     * @return HashMap<String,List<String>>
-     */
+     * Method categorizeTaxList.
+     * @param taxList List<String>
+     * @param depth int the depth of label. ex. "B.3.2.b" with depth=1, will be categorized to "B", depth=2 to "B.3",depth=3 to "B.3.2"
+     * depth=5 will not be categorized
+     * @return HashMap<String,List<String>> * @see com.minetool.tax.categorizer.ITaxCategorizer#categorizeTaxList(List<String>, int) */
     HashMap<String, List<String>> categorizeTaxList(List<String> taxList,
 	    int depth);
 
